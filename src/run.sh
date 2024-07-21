6680b04b169b34592a7c5b0e18510afeacaefdb0
neoberdin <neo6erd1n@gmail.com>
2024-07-21 16:42:28 +0300

    venv

--- src/run.sh
+++ src/run.sh
@@ -2,5 +2,7 @@
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt install python3
 sudo apt install python3-pip -y
+python3 -m venv venv
+source venv/bin/activate
 pip install qrcode
 python3 wireguard_install.py add --user test --ip 10.10.10.6
