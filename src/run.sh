910d4fc5babf7caf00000d0e2a188e5f18a8ac84
neoberdin <neo6erd1n@gmail.com>
2024-07-21 16:48:34 +0300

    apt install update

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y python3 python3-pip python3.12-venv
+sudo apt-get install -y python3 python3-pip python3.12-venv wireguard
 python3 -m venv venv
 source venv/bin/activate
 pip install qrcode
