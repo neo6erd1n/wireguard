de0a424310e682b302691209e07f21841108802b
neoberdin <neo6erd1n@gmail.com>
2024-07-21 16:37:40 +0300

    update install pip

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt install python3
-sudo apt install python3-pip
-pip install qrcode
+sudo apt install python3-pip -y
+pip install qrcode -y
 python3 wireguard_install.py add --user test --ip 10.10.10.6
