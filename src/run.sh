f26122e777322049241064269cdcfce0792b8d68
neoberdin <neo6erd1n@gmail.com>
2024-07-21 16:39:21 +0300

    update install qrcode

--- src/run.sh
+++ src/run.sh
@@ -2,5 +2,5 @@
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt install python3
 sudo apt install python3-pip -y
-pip install qrcode -y
+pip install qrcode
 python3 wireguard_install.py add --user test --ip 10.10.10.6
